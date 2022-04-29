9aad23815b4156a6467974c481dfcb171785dcfb
HelpSeed <101745564+HelpSeed@users.noreply.github.com>
2022-04-29 23:54:12 +0300

    update

--- HelpsFarmCalculate.sol
+++ HelpsFarmCalculate.sol
@@ -1,48 +1,85 @@
-
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 // SPDX-License-Identifier: MIT
 pragma solidity ^ 0.8.2;
 
 
-abstract contract HelpsFarmCalculate {
-    
- function perc(uint a, uint256 amount) internal pure returns(uint256) {
-      uint p = a*10**18/10000*100;
-      return fromWei(amount) * p / 100;
+  /**
+   * @dev created for HelpSeed Game Calculations. In this contract,
+   * there are methods that make Simple calculations such as Daily Earnings, Token to Usd, Usd to Token.
+   */
+
+  contract HelpsFarmCalculate is Ownable{
+   
+    using SafeMath for uint256;
+    uint256 internal _perc; // % Daily Earn %.
+
+  constructor(uint256 perc){
+  _perc = perc;
+  }
+  /**
+   * @dev Set Daily Earn % Value.
+   */
+  function setPerc(uint256 eperc) public onlyOwner{
+    // Minimum Limit %0.01 
+    require(eperc > 0, "Minimum 1");
+    _perc = eperc;
   }
 
+  /**
+   * @dev Returns Daily Earn % Value.
+   */
+  function getPerc() public view returns(uint256){
+    return _perc;
+  }
+
+
+  /**
+  * @dev Decimal Cleaning
+  **/
   function fromWei(uint256 amount) internal pure returns(uint256) {
       return amount / 1e18;
-  }
-
-function toWei(uint256 amount) internal pure returns(uint256) {
-      return amount*10**18;
-  }
-
-
-   function add(uint256 amount1 , uint256 amount2, uint256 p1, uint256 p2) internal pure returns(uint256){
-  return perc(p1, amount1) + perc(p2, amount2);
-  }
-
-  function calculateDailyEarn(uint256 landsCount, uint256 landPrice, uint256 pool ) internal pure returns(uint256){
-          uint256 _balance = landsCount * landPrice;
-      if (landsCount <= 50) {
-
-      return add(_balance, pool, 50, 1);// Daily %0.51
-
-    } else if (landsCount > 50 && landsCount <= 500) {
-
-       return add(_balance, pool, 67, 1); //  Daily %0.68
-
-    } else if (landsCount > 500 && landsCount <= 2500) {
-
-      return add(_balance, pool, 84, 1); // Daily %0.85
-
-    } else if (landsCount > 2500 && landsCount <= 5000) {
-
-     return add(_balance, pool, 100, 1); // Daily %1.01
-
-    } else {
-      return 0;
     }
+
+  /**
+  * @dev Decimal Convert 18-8
+  **/
+  function convertDecimals(int a) internal pure returns (uint256) {
+    return uint256(a)* (10**(18-8));
   }
+
+  /**
+  * @dev Calculate Usd <(SWAP)> Token
+  **/
+  function calculateUsdtoToken(uint256 amountUsd, uint256 tokenPriceUsd) internal pure returns(uint256){ 
+    require(tokenPriceUsd > 0 && amountUsd > 0, "Invalid Number");
+    uint256 minRequiredUSD = amountUsd*10**18; 
+    return (minRequiredUSD*10**18)/tokenPriceUsd; 
+  }
+
+  /**
+  * @dev Calculate Token <(SWAP)> Usd
+  **/
+  function calculateTokentoUsd(uint256 tokenAmount, uint256 tokenPriceUsd) internal pure returns(uint256){ 
+    require(tokenPriceUsd > 0 && tokenAmount > 0, "Invalid Number");
+    uint256 minRequiredUSD = (fromWei(tokenAmount))*tokenPriceUsd; 
+    return (minRequiredUSD) / 1; 
+  }
+
+
+  /**
+  * @dev Calculate Unlock Farm Price
+  **/
+  function calculateUnlockFarmPrice(uint256 landsCount, uint256 unlockFarmPriceToken) public pure returns(uint256){ 
+    return landsCount* unlockFarmPriceToken;
+  }
+
+  /**
+  * @dev Calculate Daily Earn Token Amount
+  **/
+  function calculateDailyEarn(uint256 landsCount, uint256 landPriceToken, uint256 pool) internal view returns(uint256){
+          uint256 _balance = (landsCount * landPriceToken) * _perc / 1000;
+          uint256 _poolEarn = pool * 1 / 1000;
+          return (_balance + _poolEarn);
+  } 
 }
