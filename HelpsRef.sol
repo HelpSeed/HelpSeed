abe2db158436f4d3b37b8ee0de5f78c999ba445f
HelpSeed <101745564+HelpSeed@users.noreply.github.com>
2022-04-29 23:55:13 +0300

    update

--- HelpsRef.sol
+++ HelpsRef.sol
@@ -2,44 +2,48 @@
 pragma solidity ^ 0.8.2;
 
 
-import "./HelpsGame.sol";
+import "./HelpsLand.sol";
 
-abstract contract HelpsRef is HelpsGame{
+ contract HelpsRef is  HelpsLand{
  
  uint256 private referenceReward;
+
+ 
 
 
   mapping(address => address[]) private refs;
   mapping(address => address) private refUser;
-  mapping(address => bool) private refExits;
+  mapping(address => bool) internal refExits;
 
  
+ /**
+ * @dev Set Reference Reward Token Amount
+ **/
  function setReferenceReward(uint256 v) public onlyOwner {
     require(v > 0, "Don't Set Zero");
     referenceReward = v * 10 ** 18;
   }
 
+ /**
+ * @dev Returns Reference Reward Token Amount
+ **/
   function getRefenceReward() internal view returns(uint256) {
       return referenceReward;
   }
 
-  function _checkRef(address a) internal view returns(bool) {
-    return refExits[a];
-  }
-
- 
-  function _setRef(address from) private {
+  /**
+  * @dev Set Reference Address And Send Reward Amount.
+  **/
+  function _setRef(address from) internal {
     require(_msgSender() != from, "False");
-    require(_checkRef(from) == false, "False");
     refs[from].push(_msgSender());
     refExits[_msgSender()] = true;
-    helps.transfer(from, getRefenceReward());
   }
 
-  function checkAndSetRef(address ref) internal {
-    _setRef(ref);
-  }
 
+  /**
+  * @dev Returns User Reference Address.
+  **/
  function getRefs(address user) public view returns(address[] memory) {
     return refs[user];
   }
